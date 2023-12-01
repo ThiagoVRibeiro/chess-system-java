0b702fe52cebf477731c50e3aeef405a83159512
Thiago Ribeiro <thvinicius1@gmail.com>
2023-12-01 19:59:39 -0300

    Correcting previous commit and Implementing possible moves of king

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,13 +27,13 @@ public class Program {
 				System.out.print("Source:");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target:");
 				ChessPosition target = UI.readChessPosition(sc); 
 				
-				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				ChessPiece captChessPiece = chessMatch.performChessMove(source, target);
 			}
 			catch(ChessException e) {
